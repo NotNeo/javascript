9cea0e684d2eb6328de145a0e6a318ffa55ac674
notneo <jamesanderson2nd@gmail.com>
2015-10-12 01:48:56 -0400

    364 days, i'm in hell oh well.

--- Chapter14/opo.js
+++ Chapter14/opo.js
@@ -1,4 +1,6 @@
-var firstName = document.forms['purchase_order']['firstName'].value;
+var po = document.getElementById('purchase_order');
+var alertString = 'You must include: ';
+var firstName = document.getElementById('purchase_order').elements.namedItem('firstName').value;
 var lastName = document.forms['purchase_order']['lastName'].value;
 var streetAddress1 = document.forms['purchase_order']['streetAddress1'].value;
 var streetAddress2 = document.forms['purchase_order']['streetAddress2'].value;
@@ -16,16 +18,15 @@ var submit = document.forms['purchase_order']['amex'].value;
 var inputs =['First Name', 'Last Name', 'Street Address 1', 'Street Address 2', 'City', 'State', 'Zip', 'Phone', 'Fax', 'Credit Card', 'Visa', 'Master Card', 'American Express'];
 
 function validateAll(){
-  for(var i = 0; i<=inputs.length -1; i++) {
-    if (inputs[i].value===null) {   
-      // alert('You must enter' + inputs[i]); 
-      alert(firstName.value);
+  for(var i = 0; i <= po.elements.length -1; i++) { 
+    if (po.elements.item(i).value===null || po.elements.item(i).value ===inputs[i]) {
+      alertString += inputs[i] + '<br>';
+      alert(alertString);
+      return false;
     }
   }
 }
 
 
-// document.addEventListener('submit', function(){ 
-//   validateAll();
-// });
+document.addEventListener('submit', validateAll());
 
