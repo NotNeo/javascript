0fa1bdcb93aafcec31f986624cb38f75571ab5e9
JGoldsmith <justingoldsmith@my.tridenttech.edu>
2015-10-12 15:50:22 -0400

    The fully working code has arrived

--- Chapter14/opo.js
+++ Chapter14/opo.js
@@ -1,23 +1,24 @@
-var po = document.getElementById('purchase_order');
-var alertString = 'You must include: ';
-var firstName = document.forms['purchase_order']['firstName'].value;
-var lastName = document.forms['purchase_order']['lastName'].value;
-var streetAddress1 = document.forms['purchase_order']['streetAddress1'].value;
-var city = document.forms['purchase_order']['city'].value;
-var state = document.forms['purchase_order']['state'].value;
-var zip = document.forms['purchase_order']['zip'].value;
-var phone = document.forms['purchase_order']['phone'].value;
-var fax = document.forms['purchase_order']['fax'].value;
-var creditCard = document.forms['purchase_order']['creditCard'].value;
-var visa = document.forms['purchase_order']['visa'].value;
-var masterCard = document.forms['purchase_order']['masterCard'].value;
-var amex = document.forms['purchase_order']['amex'].value;
-var submit = document.forms['purchase_order']['submit'];
 
-var inputs =['First Name', 'Last Name', 'Street Address 1', 'City', 'State', 'Zip', 'Phone', 'Fax', 'Credit Card', 'Visa', 'Master Card', 'American Express'];
+
+var inputs =['First Name', 'Last Name', 'Street Address 1', 'City', 'State', 'Zip', 'Phone', 'Payment Method', 'Credit Card Number', 'Credit Exp Month', 'Credit Exp Year'];
 //var validators = [validateNames, validateAddress, validateLocale];
 
-function validateAll() {
+var validateAll = function() {
+	var firstName = document.forms['purchase_order']['firstName'].value;
+	var lastName = document.forms['purchase_order']['lastName'].value;
+	var streetAddress1 = document.forms['purchase_order']['streetAddress1'].value;
+	var city = document.forms['purchase_order']['city'].value;
+	var state = document.forms['purchase_order']['state'].value;
+	var zip = document.forms['purchase_order']['zip'].value;
+	var phone = document.forms['purchase_order']['phone'].value;
+	var method = document.forms['purchase_order']['ccard'].value;
+	var ccnum = document.forms['purchase_order']['creditCardNumber'].value;
+	var expm = document.forms['purchase_order']['expMonth'].value;
+	var expy = document.forms['purchase_order']['expYear'].value;
+	var submit = document.forms['purchase_order']['submit'];
+
+	var alertString = 'You must include: ';
+
 	if(firstName == '') {
 		alertString += '\n' + inputs[0];
 		alert(alertString);
@@ -54,7 +55,36 @@ function validateAll() {
 		return false;
 	}
 
-	alert(firstName);
+	if(phone == '') {
+		alertString += '\n' + inputs[6];
+		alert(alertString);
+		return false;
+	}
+
+	if(method == '') {
+		alertString += '\n' + inputs[7];
+		alert(alertString);
+		return false;
+	}
+
+	if(ccnum == '') {
+		alertString += '\n' + inputs[8];
+		alert(alertString);
+		return false;
+	}
+
+	if(expm == 'MO') {
+		alertString += '\n' + inputs[9];
+		alert(alertString);
+		return false;
+	}
+
+	if(expy == 'YEAR') {
+		alertString += '\n' + inputs[10];
+		alert(alertString);
+		return false;
+	}
+
 }
 
-//window.onLoad=submit.addEventListener('click', validateAll());
+submit.addEventListener('click', validateAll);
